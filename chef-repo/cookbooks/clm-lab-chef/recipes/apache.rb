94e1e7d17303ff474755cd117259dd5321628e38
Corey Kaiser <ckcisco@gmail.com>
2017-02-06 09:08:04 -0600

    new file:   chef-repo/.chef/knife.rb 	new file:   chef-repo/.chef/trusted_certs/chef-server.crt 	new file:   chef-repo/.gitignore 	new file:   chef-repo/README.md 	new file:   chef-repo/cookbooks/chefignore 	new file:   chef-repo/cookbooks/clm-lab-chef/CHANGELOG.md 	new file:   chef-repo/cookbooks/clm-lab-chef/README.md 	new file:   chef-repo/cookbooks/clm-lab-chef/files/index.html 	new file:   chef-repo/cookbooks/clm-lab-chef/metadata.rb 	new file:   chef-repo/cookbooks/clm-lab-chef/recipes/apache.rb 	new file:   chef-repo/cookbooks/clm-lab-chef/recipes/default.rb 	new file:   chef-repo/cookbooks/starter/attributes/default.rb 	new file:   chef-repo/cookbooks/starter/files/default/sample.txt 	new file:   chef-repo/cookbooks/starter/metadata.rb 	new file:   chef-repo/cookbooks/starter/recipes/default.rb 	new file:   chef-repo/cookbooks/starter/templates/default/sample.erb 	new file:   chef-repo/roles/clmlab-linux-role.rb 	new file:   chef-repo/roles/starter.rb

---
+++ chef-repo/cookbooks/clm-lab-chef/recipes/apache.rb
@@ -0,0 +1,22 @@
+#Install Apache Web Server
+package 'httpd' do
+  action :install
+end
+
+service 'httpd' do
+  action [ :enable, :start ]
+end
+
+cookbook_file "/var/www/html/index.html" do
+  source "index.html"
+  mode "0644"
+end
+execute 'httpd_firewall' do
+  command '/usr/bin/firewall-cmd  --permanent --zone public --add-service http'
+  ignore_failure true
+end
+
+execute 'reload_firewall' do
+  command '/usr/bin/firewall-cmd --reload'
+  ignore_failure true
+end
---
+++ chef-repo/roles/clmlab-linux-role.rb
@@ -0,0 +1,3 @@
+name "clm-lab-linux"
+description "CLM LAB Workstation Role"
+run_list "recipe[clm-lab-chef::apache]"
